28778c8fac013ce3015c475260f1969fe9507bcf
Tom Endacott <tendacott@spartaglobal.com>
2018-07-01 16:24:38 +0100

    Added further BMI calc functions with imperial, metric and a way to choose

--- calc.js
+++ calc.js
@@ -2,11 +2,11 @@
 // Which calculator
 var whichCalc = prompt("Would you like to use the: \n(a)dvanced[+ - * / power sq-root] \n(b)asic calculator[+ - * /] \nB(M)I calculator?");
 
-if(whichCalc == "b" || whichCalc == "basic") {
+if(whichCalc.toLowerCase() == "b" || whichCalc.toLowerCase() == "basic" ) {
   basic();
-} else if(whichCalc == "a" || whichCalc == "adv" || whichCalc == "advanced") {
+} else if(whichCalc.toLowerCase() == "a" || whichCalc.toLowerCase() == "adv" || whichCalc.toLowerCase() == "advanced") {
   advanced();
-} else if(whichCalc == "m" || whichCalc == "bmi") {
+} else if(whichCalc.toLowerCase() == "m" || whichCalc.toLowerCase() == "bmi") {
   bmi();
 }
 
@@ -62,10 +62,16 @@ function advanced() {
 //BMI Calculator
 function bmi() {
   var measure = prompt("Would you like to use (m)etric or (i)mperial measurements?");
-  var heightImp = parseInt(prompt("Enter your height in inches"));
-  var weightImp = parseInt(prompt("Enter your weight in pounds"));
-  
-  alert("Your BMI is " + (703*weight)/(Math.pow(height, 2)));
+
+  if(measure.toLowerCase() == "m" || measure.toLowerCase() == "metric") {
+    var heightMet = parseInt(prompt("What is your weight in kilograms?"));
+    var weightMet = parseInt(prompt("What is your height in centimetres?"));
+    alert("Your BMI is " + (1000*heightMet)/(Math.pow(heightMet, 2)));
+  } else if(measure.toLowerCase() == "i" || measure.toLowerCase() == "imperial") {
+    var heightImp = parseInt(prompt("What is your weight in pounds?"));
+    var weightImp = parseInt(prompt("What is your height in inches?"));
+    alert("Your BMI is " + (703*weight)/(Math.pow(height, 2)));
+  }
 }
 
 
