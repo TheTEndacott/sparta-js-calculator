0a891d251283613599e25fb7dfdea9c3f3354a51
Tom Endacott <tendacott@spartaglobal.com>
2018-07-01 15:45:24 +0100

    Advanced calculator added, square root not yet working

--- calc.js
+++ calc.js
@@ -23,8 +23,37 @@ function basic() {
 	}
 }
 
+// Advanced Calculator
+function advanced() {
+  var firstNum = parseInt(prompt("What is your first number?"));
+  var action = prompt("What action would you like to take? (+)addition (-)subtraction (*)multiplication (/)division (p)power of (s)square root");
+	var secondNum = parseFloat(prompt("What is your second number? \nLeave blank for a square root calculation"));
+
+  switch(action) {
+    case "+":
+  		alert(firstNum+secondNum);
+  		break;
+		case "-":
+  		alert(firstNum-secondNum);
+  		break;
+		case "*":
+  		alert(firstNum*secondNum);
+  		break;
+		case "/":
+  		alert(firstNum/secondNum);
+  		break;
+    case "p":
+      alert(firstNum**secondNum);
+      break;
+    case "s":
+      alert(sqrt("firstNum"));
+      break;
+  }
+}
 
 // Which calculator
 if(whichCalc == "b" || whichCalc == "basic") {
   basic();
-} 
+} else if(whichCalc == "a" || whichCalc == "adv" || whichCalc == "advanced") {
+  advanced();
+}
